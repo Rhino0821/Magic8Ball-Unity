be11246032449a361277a15fb6b32ec0c3cfbda9
Rhino0821 <ryananimates0803@gmail.com>
2025-10-05 22:26:51 -0500

    Optimism Slider Update

--- Magic8Ball/Assets/Scripts/Magic8Ball.cs
+++ Magic8Ball/Assets/Scripts/Magic8Ball.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 public class Magic8Ball : MonoBehaviour
 {
     public TextMeshProUGUI answerText;
+    public UnityEngine.UI.Slider optimismSlider;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && optimismSlider.value <= 0.5f)
         {
-            int index = Random.Range(0, answer.Length);
-            
-            answerText.text = answer[index];
+            int index2 = Random.Range(0, answerPositive.Length);
+
+            answerText.text = answerPositive[index2];
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int index = Random.Range(0, answerNegative.Length);
+
+            answerText.text = answerNegative[index];
         }
     }
-    private string[] answer = { "Yes", "No", "Maybe", "Try Again", "Perchance", "Nah", "Leave Me Alone", "*magic*" };
+
+    private string[] answerNegative = { "No", "Nuh Uh", "Nah", "Leave Me Alone", };
+    private string[] answerPositive = { "Yes", "Yer", "Yuh", "*magic*" };
 }
